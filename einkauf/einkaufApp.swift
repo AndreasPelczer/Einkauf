c4c3182e2c71587b532f7dca51c2098430e4d706
Andreas Pelczer <a.pelczer@gmail.com>
2023-11-24 10:09:57 +0100

    Initial Commit

---
+++ einkauf/einkaufApp.swift
@@ -0,0 +1,17 @@
+//
+//  einkaufApp.swift
+//  einkauf
+//
+//  Created by Andreas Pelczer on 24.11.23.
+//
+
+import SwiftUI
+
+@main
+struct einkaufApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
